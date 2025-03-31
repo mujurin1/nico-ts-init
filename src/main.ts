0f70b8f51c177055efdd41d04376af10743c1afd
mujurin1 <mujurin25@yahoo.co.jp>
2025-03-31 19:15:28 +0900

    余計なエンティティを削除

--- src/main.ts
+++ src/main.ts
@@ -14,25 +14,5 @@ function main(gameMainParam: g.GameMainParameterObject): void {
     });
     // ★青色の四角形をシーンに追加します
     scene.append(blueRect);
-
-
-
-    // ★紫色の四角形を作成します
-    const purpleRect = new g.FilledRect({
-      scene,
-      cssColor: "purple",
-      width: 100, height: 100,
-      x: 50, y: 100,
-      touchable: true,
-    });
-    // ★紫色の四角形をシーンに追加します
-    scene.append(purpleRect);
-
-    // ★紫色の四角形がクリックされた時のイベントを追加します
-    purpleRect.onPointDown.add(e => {
-      // ★紫色の四角形を右方向へ +10 だけ移動します
-      purpleRect.x += 10;
-      purpleRect.modified();
-    });
   }
 };
